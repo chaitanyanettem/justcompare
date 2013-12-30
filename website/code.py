35995b60041ae407c4847d22efc7cf9200fe94d1
chaitanyanettem <chaitanyanettem@gmail.com>
2013-12-29 19:01:27 -0800

    changes to website

--- website/code.py
+++ website/code.py
@@ -2,6 +2,7 @@ import web
 import re
 import urllib2
 import json
+import traceback
 
 render = web.template.render('/var/www/justcompare/templates/')
 db = web.database(dbn = 'postgres', user = 'chaitanya', pw = 'nettem', db='compare')
@@ -35,20 +36,18 @@ class search:
 #where fts @@ to_tsquery ('english','fashion')
 #order by rank desc;	
 		what_essential = "isbn,book_name,author_name,"
-		what_prices    = "hs18_price,fk_price,infibeam_price,indiaplaza_price,crossword_price,bookadda_price,landmark_price,"
-		what_images    = "hs18_img_src,fk_img_src,infibeam_img_src,indiaplaza_img_src,crossword_img_src,bookadda_img_src,"
-		what_links     = "hs18_booklink,fk_booklink,infibeam_booklink,indiaplaza_booklink,bookadda_booklink,crossword_booklink,landmark_booklink,"
+		what_prices    = "min_price,"
 		what_misc      = "fts, ts_rank(fts,to_tsquery('english', '{0}'), 1) AS rank".format(query)
-		what_total     = what_essential+what_prices+what_images+what_links+what_misc
+		what_total     = what_essential+what_prices+what_misc
 		whereq 		   = "fts @@ to_tsquery('english','{0}')".format(query) 
 		#myvar = dict('lang':'english','query':'input_q','normalization':1)
-		ordered = db.select('books_bookdata', what=what_total,where=whereq,order="rank DESC", limit=20)
+		ordered = db.select('books_bookdata', what=what_total,where=whereq,order="rank DESC", limit=30)
 		return render.search(input_q.q, ordered)
 
 class book:
 	def GET(self, isbn):
 		what_essential = "isbn,book_name,author_name,"
-		what_prices    = "hs18_price,fk_price,infibeam_price,indiaplaza_price,crossword_price,bookadda_price,landmark_price,"
+		what_prices    = "hs18_price,fk_price,infibeam_price,indiaplaza_price,crossword_price,bookadda_price,landmark_price,min_price,"
 		what_images    = "hs18_img_src,fk_img_src,infibeam_img_src,indiaplaza_img_src,crossword_img_src,bookadda_img_src,"
 		what_links     = "hs18_booklink,fk_booklink,infibeam_booklink,indiaplaza_booklink,bookadda_booklink,crossword_booklink,landmark_booklink"
 		what_total     = what_essential+what_prices+what_images+what_links
@@ -59,8 +58,16 @@ class book:
 		try:
 			gbooks = json.load(urllib2.urlopen(gbooks))
 			descr = gbooks['items'][0]['volumeInfo']['description']
-			if len(descr)>300:
-				descr = descr[:300]+"..."
+			if len(descr)>350:
+				i = 350
+				while True:
+					if descr[i] == '.':
+						descr = descr[:i]
+						break
+					if i>355:
+						descr = descr[:i]+"..."
+						break
+					i += 1
 		except:
 			descr="Sorry! Description currently unavailable."
 		return render.book(book_details,descr)
